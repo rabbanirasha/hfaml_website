46c415b1213662edc4ec2085759de82b7fee1872
Rabbani Rasha <rabbanirasha@gmail.com>
2026-08-13 16:30:11 +0600

    production index.php updated

--- deploy/index.production.php
+++ deploy/index.production.php
@@ -1,3 +1,20 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../hfasset_website/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
 require __DIR__.'/../hfasset_website/vendor/autoload.php';
-// ...
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
 $app = require_once __DIR__.'/../hfasset_website/bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
