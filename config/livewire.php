fb7bf4c1daeead601b7438ce2ed54e5288c4c81b
Rabbani Rasha <rabbanirasha@gmail.com>
2026-08-24 13:04:37 +0600

    added pdf

--- config/livewire.php
+++ config/livewire.php
@@ -135,7 +135,7 @@ return [
         'middleware' => 'throttle:60,1',
         'preview_mimes' => [
             'png','gif','bmp','svg','wav','mp4','mov','avi','wmv','mp3','m4a',
-            'jpg','jpeg','mpga','webp','wma',
+            'jpg','jpeg','mpga','webp','wma','pdf',
         ],
         'max_upload_time' => 5, // minutes — bump if large files time out mid-upload
         'cleanup' => true,
